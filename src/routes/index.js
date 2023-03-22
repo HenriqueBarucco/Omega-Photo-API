deb299a5a1b256d3e9690f338772aa5a321b2227
Henrique Barucco <henrique_barucco@hotmail.com>
2023-03-22 14:36:58 -0300

    fix: git config

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import users from "./usersRoutes.js";
 
 const routes = (app) => {
     app.route("/").get((req, res) => {
-        res.status(200).send({ titulo: "OmegaPhotoAPI :)" });
+        res.status(200).send({ titulo: "OmegaPhotoAPI" });
     });
 
     app.use(express.json(), users);
